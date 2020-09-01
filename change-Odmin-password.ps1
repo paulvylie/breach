6f86c32252f5acf9f356043d9c1ac7ee5c2f8c72
paulvylie <55334414+paulvylie@users.noreply.github.com>
2020-09-01 19:52:19 +0300

    Update change-Odmin-password.ps1

--- change-Odmin-password.ps1
+++ change-Odmin-password.ps1
@@ -2,7 +2,7 @@ $publicip = (Invoke-WebRequest -uri "http://ifconfig.me/ip" -UseBasicParsing).Co
 
 $lastpublicipdigit = $publicip.Substring($publicip.get_Length()-1)
 
-$pass = ""
+$pass = "mOGu6nQn+"
 $password = $pass+$lastpublicipdigit
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 
